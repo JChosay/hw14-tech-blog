8283862d7671104f95a2b5e31b15eab235500108
Joshua Chosay <jjchosay@gmail.com>
2021-07-31 11:00:38 -0500

    To hell with this assignment, literally. Those who assigned it are rotten right down to their cores.

--- controllers/index.js
+++ controllers/index.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
-const dashboard = require("./dashboard")
+const dashboard = require('./userRoutes');
 
-router.use('/', homeRoutes);
-router.use('/api', apiRoutes);
-router.use("/profile", dashboard); 
+router.use('/user', homeRoutes);
+router.use('/post', postRoutes);
+router.use('/comment', commentRoutes);
 
 module.exports = router;
